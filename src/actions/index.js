d1f1ce6f953ec561020bdbc1f0192c7c5c0c9087
SuzuOkumura <44101bwdive@gmail.com>
2022-06-21 18:04:43 +0900

    implement event new component

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 /**
  * Action type
  */
-export const READ_EVENTS = 'READ_EVENTS'
+ export const READ_EVENTS = 'READ_EVENTS'
+ export const CREATE_EVENT = 'CREATE_EVENT'
 
 /**
  * Action creator 
@@ -13,4 +14,9 @@ const QUERYSTRING = '?token=token123'
 export const readEvents = () => async dispatch => {
     const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
     dispatch({type: READ_EVENTS, response})
+}
+
+export const postEvent = values => async dispatch => {
+    const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values)
+    dispatch({type: CREATE_EVENT, response})
 }
